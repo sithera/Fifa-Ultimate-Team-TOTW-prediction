32c9340c9cf1e53dc4d5c4900dad8c231c049da9
Eryk Leniart <eryk.leniart@motorolasolutions.com>
2017-06-12 02:46:49 +0200

    first SVM classifications

--- data/data_analyse.py
+++ data/data_analyse.py
@@ -4,6 +4,7 @@
 import sqlite3
 import sys
 import numpy as np
+from sklearn import svm
 from sklearn.decomposition import PCA
 import json
 import csv
@@ -46,14 +47,14 @@ class DataAnalyzer(object):
         mylist = filter(lambda x : x[0] == 3965, self.Y)
         #print mylist
 
-    def handle_json(self, fixture, filename='team_of_the_week.json'):
+    def handle_json(self, year, fixture, filename='team_of_the_week.json'):
         with open(filename) as data_file:
             data = json.load(data_file)
             #print data["data"]
             #pprint(data[0]["data"][fixture-1]["fixture"])
             #pprint(data[0]["data"][fixture-1]["players"])
 
-        return data[0]["data"][fixture-1]["players"]
+        return data[year-2015]["data"][fixture-1]["players"]
 
     def find_player_id(self, name):
         #print [self.rows[i] for i in range(len(self.rows))]
@@ -61,7 +62,7 @@ class DataAnalyzer(object):
         return player_data[0][0]
 
     def update_if_in_TOtw(self, year, fixture):
-        data = self.handle_json(fixture)
+        data = self.handle_json(year, fixture)
         players = []
         for position in data[0]:
             for player in data[0][position]:
@@ -105,11 +106,11 @@ class DataAnalyzer(object):
         return filter(lambda x : datetime.strptime(x[1], '%Y-%m-%d') > datetime(start_year, 7,7) and datetime.strptime(x[1], '%Y-%m-%d') < datetime(start_year+1, 7, 7), candidates)
 
     def update_all_players(self, year):
-        fixtures = range(1,38)
+        fixtures = range(36,37)
         for i in fixtures:
             self.update_if_in_TOtw(year, i)
-        self.save_to_file('data_calculated', self.Y)
-        
+        self.save_to_file('data_calculated' + str(year), self.Y)
+
     def save_to_file(self, filename, content):
         filename = filename + ".csv"
         with open(filename, 'wb') as file:
@@ -124,6 +125,35 @@ class DataAnalyzer(object):
 
     #spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
     #spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
+    def run_svm(self):
+        Xsvm = []
+        ysvm = []
+        for i in range(len(self.Y)):
+            if i < 100:
+                continue
+            Xsvm.append(self.Y[i][3:6])
+            print self.Y[i][3:6]
+            ysvm.append(self.Y[i][6])
+
+        #print Xsvm
+        #print ysvm
+
+
+        clf = svm.SVC()
+        clf.fit(Xsvm,ysvm)
+
+        print clf.predict([[95.46998874785378,	-10.455099879722816,	8.249285784045766]])
+        print clf.predict([[-11.911608306775511,	-4.960642606125528,	-21.248186559307268]])
+        print clf.predict([[-84.96169891257794,	14.995920700196903,	16.34380187691025]])
+        print clf.predict([[-61.5396615043822,	-4.719625078226473,	0.9947794822283846]])
+        print clf.predict([[49.17837859606411,	33.93099009166503,	-3.2989638896094835]])
+        print clf.predict([[-36.088040844332426,	-16.437124809782212,	-23.30283250693931]])
+        print clf.predict([[23.071458306015284,	6.3895150724014504,	-15.641327844575892]])
+        print clf.predict([[0.5474392044500311,	19.583617357003405,	-13.070166080634563]])
+        print clf.predict([[125.79422002401283,	81.08243862949172,	-15.579239375631678]])
+        print clf.predict([[-58.92553677984468,	-5.709797335438783,	-19.555997730779442]])
+        print clf.predict([[134.87989156562023,	-51.14799314269806,	44.93611270473487]])
 
 
 
@@ -141,8 +171,9 @@ if __name__ == "__main__":
     #print data.Y
     #data.slice_by_season(2015)
 	
-    data.update_all_players(2015)
+    data.update_all_players(2016)
+    data.run_svm()
     #print "plaers ids"
-    #print data.find_player_id("Robbie Brady")
+    #print data.find_player_id("Michael Keane")
     
 
